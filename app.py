8614e9b8f13fbaa146b9f2ab3c737d3daaf50819
Ambiental Kenzo Jeans <ambientalkenzo@gmail.com>
2026-09-09 09:50:15 -0500

    app.py

--- app.py
+++ app.py
@@ -471,7 +471,7 @@ with tab2:
             )
 
     fig.update_layout(
-        height=950,
+        height=1250,
         margin=dict(l=130, r=40, t=180, b=80),
         paper_bgcolor="#0e1117",
         plot_bgcolor="#0e1117",
